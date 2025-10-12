959fb39a4e28ebe0f3657023d9f991d312f255ce
Oğuz <132291321+elrondforwin@users.noreply.github.com>
2025-10-12 23:56:55 +0300

    added wallpaper switcher with rofi and a little script

---
+++ .config/hypr/scripts/wallchanger.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+WALL_DIR="/home/$USER/Pictures/wallpapers"
+
+check_preq() {
+  if [[ -z $(command -v swww) ]]; then
+    notify-send "Cannot find 'swww' exiting..."
+    exit 1
+  fi
+
+  if [[ -z $(pidof swww-daemon) ]]; then
+    notify-send "'swww-daemon' is not running. Exiting..."
+    exit 1
+  fi
+
+  if [[ -z $(command -v rofi) ]]; then
+    notify-send "Cannot find 'rofi' exiting..."
+    exit 1
+  fi
+
+  if [[ ! -d "$WALL_DIR" ]]; then
+    notify-send "Cannot find wallpapers directory. Exiting..."
+    exit 1
+  fi
+}
+
+_swww() {
+  swww img --transition-type center --transition-step 90 --transition-duration 2 --transition-fps 165 "$1"
+}
+
+walls() {
+  WALLS=$(find "$WALL_DIR" -mindepth 1 -maxdepth 1 -type f -printf "%f\n")
+  if [[ -z "$WALLS" ]]; then
+    notify-send "Cannot find any wallpapers in wallpapers directory"
+    exit 0
+  else
+    SELECTED=$(for a in $WALL_DIR/*; do
+      echo -en "$a\0icon\x1f$a\n"
+    done | rofi -dmenu -show-icons -theme wallpapers.rasi)
+
+    if [[ -z "$SELECTED" ]]; then
+      exit 0
+    else
+      SEL_WAL_ALONE=$(echo "$SELECTED" | sed "s|$WALL_DIR/||g")
+      notify-send "Selected wallpaper: $SEL_WAL_ALONE"
+      _swww "$SELECTED"
+    fi
+  fi
+}
+
+main() {
+  check_preq
+  walls
+}
+
+main
--- .config/rofi/scripts/powermenu.sh
+++ .config/rofi/scripts/powermenu.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Rofi teması
 ROFI_CMD="rofi -dmenu -p Power -theme ~/.config/rofi/powermenu/powermenu.rasi"
 
 shutdown='󰐥'
@@ -11,23 +10,22 @@ logout=''
 yes=''
 no=''
 
-# Menü seçenekleri
 options="$shutdown\n$reboot\n$suspend\n$logout"
 
 chosen="$(echo -e "$options" | $ROFI_CMD)"
 
 case "$chosen" in
-    "$shutdown")
-        ~/.config/rofi/scripts/power.sh shutdown
-        ;;
-    "$reboot")
-        ~/.config/rofi/scripts/power.sh reboot
-        ;;
-    "$suspend")
-        ~/.config/rofi/scripts/power.sh suspend
-        ;;
-    "$logout")
-        ~/.config/rofi/scripts/power.sh exit
-        ;;
+"$shutdown")
+  ~/.config/rofi/scripts/power.sh shutdown
+  ;;
+"$reboot")
+  ~/.config/rofi/scripts/power.sh reboot
+  ;;
+"$suspend")
+  ~/.config/rofi/scripts/power.sh suspend
+  ;;
+"$logout")
+  ~/.config/rofi/scripts/power.sh exit
+  ;;
 
 esac
