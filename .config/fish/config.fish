ef3b3634f37da2960fdc1746744e81f2f2d802fc
Oğuz <132291321+elrondforwin@users.noreply.github.com>
2025-10-25 18:46:02 +0300

    new fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -137,6 +137,40 @@ alias rip="expac --timefmt='%Y-%m-%d %T' '%l\t%n %v' | sort | tail -200 | nl"
 zoxide init fish | source
 alias cd="z"
 
+# prompt
+function fish_prompt --description 'Write out the prompt'
+    set -l last_status $status
+    set -l normal (set_color normal)
+    set -l status_color (set_color brgreen)
+    set -l cwd_color (set_color $fish_color_cwd)
+    set -l vcs_color (set_color brpurple)
+    set -l prompt_status ""
+
+    # Since we display the prompt on a new line allow the directory names to be longer.
+    set -q fish_prompt_pwd_dir_length
+    or set -lx fish_prompt_pwd_dir_length 0
+
+    # Color the prompt differently when we're root
+    set -l suffix '>'
+    if functions -q fish_is_root_user; and fish_is_root_user
+        if set -q fish_color_cwd_root
+            set cwd_color (set_color $fish_color_cwd_root)
+        end
+        set suffix '#'
+    end
+
+    # Color the prompt in red on error
+    if test $last_status -ne 0
+        set status_color (set_color $fish_color_error)
+        set prompt_status $status_color "[" $last_status "]" $normal
+    end
+
+    echo ' '
+    echo -s $cwd_color (prompt_pwd) $vcs_color (fish_vcs_prompt) $normal ' ' $prompt_status
+    echo -n -s $status_color $suffix ' ' $normal
+end
+
+# yazi init
 function y
     set tmp (mktemp -t "yazi-cwd.XXXXXX")
     yazi $argv --cwd-file="$tmp"
@@ -175,5 +209,6 @@ alias reloadwaybar="pkill waybar && waybar & disown"
 alias vim="nvim"
 alias vi="vim"
 alias v="vim"
+alias img="kitten icat"
 
 alias code="code --enable-features=UseOzonePlatform --ozone-platform=wayland"
